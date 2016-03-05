084e107751250dd85722c07902d62fe17e66ae4f
CPapadim <papadimitriou.c@gmail.com>
2016-03-04 21:28:01 -0600

    Update loadSpikes.m

--- generic/loadSpikes.m
+++ generic/loadSpikes.m
@@ -26,20 +26,4 @@ function [Spikes] = loadSpikes(spikefile)
 %%%%%PLEXON - NEED TO WRITE OWN loadSpikesPlexon function since loading from plexon needs a totally different set of parameters than loading from reach files
 
 
-	%%% Loop through all LFP files again
-%        	data = load(spikefile,'tscounts','allts'); %Need spike times if plexon
-%        	dataChannels = find(data.tscounts(2,:)>1)-1;
-%        	spikeTimes = []; %spike times
-%
-%        	spikeTimes= data.allts{2,dataChannels(electrode)}; %In seconds
-		%%% Add (lfpsize/LFPSamplingRate)    to spikeTimes for every file
-	%%% LFP FILE LOOP END
-        
-%	spike_vect = zeros((max(TrialTimes)+time_window)*Fs,1);
-%        spike_vect(round(spikeTimes*SamplingRate)) = 1;
-%
-%        for i = 1:length(TrialTimes)
-%                AlignedSpikeTrials(i,:) = spike_vect((TrialTimes(i)+((starttimes(i):starttimes(i)+window)))*Fs)
-%        end
-
 end
