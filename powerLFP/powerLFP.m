35bbbd1819d1cab049f8fba8c1419cacee6f31bd
CPapadim <papadimitriou.c@gmail.com>
2016-03-04 21:22:46 -0600

    Update powerLFP.m

--- powerLFP/powerLFP.m
+++ powerLFP/powerLFP.m
@@ -15,46 +15,9 @@ warning off all
 % Import Parameters
 Params = readParams('Parameters.temp');
 
-%commandline_args=regexp(char(commandline{1}),'\s+','split');
-%
-%%%  Extract Reach FileName %%
-%slashes=strfind(char(commandline_args{end-1}),'/');
-%if(isempty(slashes))
-%        lastslash=1;
-%else
-%        lastslash=slashes(end);
-%end
-%reachfile=char(commandline_args{end-1}(lastslash+1:end));
-%reachfullfile=char(commandline_args{end-1});
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-
-%% SET DEFAULTS IF ARGUMENT DOESN'T EXIST
-%if exist('time_interval') == 0
-%    time_interval = 500.0;
-% end
-%if exist('time_window') == 0
-%   time_window = 200.0;
-% end
-%if exist('freq_start') == 0
-%   freq_start = 30.0;
-% end
-%if exist('freq_end') == 0
-%   freq_end = 60.0;
-% end
-%if exist('freq_step') == 0
-%   freq_step = 10.0;
-% end
-%if freq_step==0
-%	freq_step=1;
-%end
 
 freq=Params.freqStart1:Params.freqStep1:Params.freqEnd1;
-%time_step=double(round(time_window/4)); %Hardcode time step in spectral analysis to equal 1/4 of the time window
 time_step=double(Params.time1)/4; %Hardcode time step in spectral analysis to equal 1/4 of the time window
-%smoothspan=double(round(smoothSpan/time_step))
 smoothspan=double(Params.filter/Params.timeInterval); % For lowess, span needs to be a fraction of total time
 
 
@@ -70,10 +33,6 @@ smoothspan=double(Params.filter/Params.timeInterval); % For lowess, span needs t
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 [power_array]=powerSpectrum(AlignedLFPTrials,LFPSamplingRate,Params.time1,time_step,freq); %Generate Power Spectrum
-%%% 	Normalize the Power Spectrum	%%%
-%power_normalization=repmat(squeeze(mean(mean(power_array,1),3)),[size(power_array,1),1,size(power_array,3)]); %Normalize Power array by the mean value of each trial and time, for each frequency band - ONLY USED IN DEFAULT FIGS.
-%power_array_normalized=power_array./power_normalization;
-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -88,9 +47,6 @@ for i=typelist
 	
 	powerSorted(typecount,:,:,1)=squeeze(mean(power_array(find(TrialType==i),:,:),1));
 	powerSorted(typecount,:,:,2)=squeeze(std(power_array(find(TrialType==i),:,:),[],1));
-	
-	%powerSortedNorm(typecount,:,:,1)=squeeze(mean(power_array_normalized(find(TrialType==i),:,:),1));
-	%powerSortedNorm(typecount,:,:,2)=squeeze(std(power_array_normalized(find(TrialType==i),:,:),[],1));
 	
 	N=[N size(power_array(find(TrialType==i),:,:),1)];
 	
@@ -131,50 +87,6 @@ end
 
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%                     FIGURES                      %%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%%%%%%%%%%%%%% Power - Frequency vs. Time %%%%%%%%%%%%%%%%%%%%%%%%
-%h=figure;
-%power_means=squeeze(mean(power_array_normalized,1));
-%imagesc(((time_window/2):time_step:(time_interval-time_window/2)),freq,power_means);
-%freqstep=floor(length(freq)/10);
-%if(freqstep>0)
-%	set(gca,'YTick',freq(1:freqstep:end));
-%	set(gca,'YTickLabel',freq(1:freqstep:end));
-%else
-%	set(gca,'YTick',freq);
-%	set(gca,'YTickLabel',freq);
-%end
-%colorbar
-%title('Normalized Power :::  Frequency vs. Time');
-%ylabel('Frequency (Hz)');
-%xlabel('Time (ms)');
-%print(h,'-dpsc2','-loose','power.ps');
-%
-%%%%%%%%%%%%% Power - Trial Type vs. Time %%%%%%%%%%%%%%%%%%%%%%%%
-%h=figure;
-%pt_means=squeeze(mean(powerSortedNorm(:,:,:,1),2));
-%imagesc(((time_window/2):time_step:(time_interval-time_window/2)),1:length(typelist),pt_means);
-%set(gca,'YTick',1:length(typelist));
-%set(gca,'YTickLabel',typelist);
-%colorbar
-%title('Normalized Power ::: Trial Type vs Time');
-%ylabel('Trial Type');
-%xlabel('Time (ms)');
-%print(h,'-dpsc2','-loose','-append','power.ps');
-%
-%%%%%%%%%%%%% Power vs. Trial Type %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%h=figure;
-%ptf_means=squeeze(mean(mean(powerSortedNorm(:,:,:,1),2),3));
-%plot(typelist,ptf_means,'-o','LineWidth',5,'MarkerSize',10,'MarkerFaceColor','k');
-%set(gca,'XTick',typelist);
-%title('Mean Power vs. Trial Type');
-%xlabel('Trial Type');
-%ylabel('Power');
-%print(h,'-dpsc2','-loose','-append','power.ps');
-%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % Data Output		%
